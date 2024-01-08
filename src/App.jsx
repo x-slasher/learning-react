c443661a699a798b5ae991cfcfbced9521965b7a
Nayeem <nayeem.hasanewu@gmail.com>
2024-01-08 15:24:29 +0600

    workin on form

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,13 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = (expenses) => {
+    console.log('in app.js');
+    console.log(expenses);
+  }
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
